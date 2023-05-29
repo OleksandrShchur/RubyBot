17e24daf30d440b2a211bf39236ed80f7de12f4c
IrynaDorosh <umwelt@ukr.net>
2023-05-29 08:29:58 +0300

    Removed bin from gitignore

---
+++ bin/runner.rb
@@ -0,0 +1,21 @@
+require_relative "../lib/load_libraries"
+
+Root_Path = File.join(File.dirname{__FILE__}, "../")
+
+Quiz.config do |quiz|
+ quiz.yaml_dir = File.join(Root_Path, "config", "quiz_yml")
+ quiz.answers_dir = File.join(Root_Path, "quiz_answers")
+ quiz.log_dir = File.join(Root_Path, "log")
+end
+
+
+
+QuizBot.config do |quiz_bot|
+   quiz_bot.yaml_dir = File.join(Root_Path, "config")
+   quiz_bot.log_dir = File.join(Root_Path, "log")
+ end
+
+engine = QuizBot::EngineBot.new
+engine.r
+
+
